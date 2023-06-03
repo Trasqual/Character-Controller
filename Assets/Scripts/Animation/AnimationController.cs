3467b881abd78f9e50d86fa5de0045e015551c69
trasqual <tolgaakad@gmail.com>
2023-06-03 15:22:24 +0300

    Added animations and mock up character

---
+++ Assets/Scripts/Animation/AnimationController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    [SerializeField] private AnimationController _animCont;
+
+    public void PlayMovementAnimation()
+    {
+
+    }
+
+    public void PlayJumpAnimation()
+    {
+
+    }
+
+    public void PlayFallAnimation()
+    {
+
+    }
+
+    public void PlayDodgeAnimation()
+    {
+
+    }
+}
